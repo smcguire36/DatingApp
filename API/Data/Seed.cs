42ddea67cf3c5cc0563f55ba8a7d418df3d97f9b
Stewart McGuire <smcguire36@gmail.com>
2020-10-30 23:49:10 -0400

    More for Section 8

---
+++ API/Data/Seed.cs
@@ -0,0 +1,36 @@
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+using API.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Data
+{
+    public class Seed
+    {
+        public static async Task SeedUsers(DataContext context)
+        {
+            // If there are any users, return without doing anything
+            if (await context.Users.AnyAsync()) return;
+
+            var userData = await System.IO.File.ReadAllTextAsync("Data/UserSeedData.json");
+            var users = JsonSerializer.Deserialize<List<AppUser>>(userData);
+            if (users == null) return;
+            foreach (var user in users)
+            {
+                using var hmac = new HMACSHA512();
+
+                user.UserName = user.UserName.ToLower();
+                user.PasswordSalt = hmac.Key;
+                user.PasswordHash = hmac.ComputeHash(Encoding.UTF8.GetBytes("ncc-1701"));
+
+                await context.Users.AddAsync(user);
+            }
+
+            await context.SaveChangesAsync();
+        }
+    }
+}
